3f5375a23fe858430c6c3bd0138cae35e4b4fd47
jalal Arif <jalalharfjalal@gmail.com>
2020-05-17 19:28:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 
 
 document.addEventListener("DOMContentLoaded", function() {
+  let str=document.getElementById("text").innerHTML="Jalal Arif Jalal";
+  
   console.log("The DOM has loaded");
 });
  
-console.log(
-  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
-);
